2e7a26efd0bed4dda5618ddad6e75e12a14834c7
Ethan Frank <edf32@case.edu>
2022-10-04 13:57:45 -0400

    Improve reliability of morse code and timing interactions

--- main/main.ino
+++ main/main.ino
@@ -103,9 +103,24 @@ class MorseCodeRepeater {
   }
 };
 
-int OUTPUT_PIN = 4;
+// CONSTANTS
+int OUTPUT_PIN = 12;
 int MORSE_PIN = 3;
 int INPUT_PIN = A0;
+
+long BUFFER_DELAY_TIME = 2000l;  // 2 seconds, in ms
+long CUTOFF_PERIOD_TIME = 3 * 60 * 1000;  // 3 minutes, in ms
+
+long MORSE_REPEAT_TIME = 8000L;
+int MORSE_FREQUENCY = 700;
+String MORSE_STRING = "VV";
+
+int RECEIVER_ON_THRESHOLD = 400;  // Analog read out of 1024
+
+// Quindar tone for turning off https://en.m.wikipedia.org/wiki/Quindar_tones
+long QUINDAR_OFF_DURATION = 250; // ms
+int QUINDAR_OFF_FREQUENCY = 2475;
+boolean QUINDAR_ENABLED = true;
 
 int inval;  // Raw on/off from LED
 int cutoffval;  // Value processed to turn off after certain time
@@ -115,16 +130,14 @@ int bufferval;  // Value processed to stay on a bit after input turns off
 int last_inval;
 int last_bufferval;
 
-long buffer_delay_time = 2000;  // ms
-long cutoff_delay_time = 6000;
-
 
 int outval;  // Output to transmitter
+int last_outval;
 
 unsigned long start_time;
 unsigned long cutoff_start_time; 
 
-MorseCodeRepeater morseCodeRepeater(MORSE_PIN, 12000L , 700, "VV");
+MorseCodeRepeater morseCodeRepeater(MORSE_PIN, MORSE_REPEAT_TIME , MORSE_FREQUENCY, MORSE_STRING);
 
 void setup() {
   Serial.begin(9600);
@@ -134,10 +147,25 @@ void setup() {
 void loop() {
   // Input value from receiving baofeng
   int sensorValue = analogRead(INPUT_PIN);
-  if (sensorValue > 400) {
+  if (sensorValue > RECEIVER_ON_THRESHOLD) {
     inval = 1;
   } else {
     inval = 0;
+  }
+
+  // Morse code is a blocking call. Insert this here so it can simulate the 
+  // inval being set to 1, and the start times can be set appropriatley.
+  // We do many hacks with the other variables to make them behave properly.
+  if (morseCodeRepeater.hasTimeElapsed()) {
+      Serial.print("Playing Morse Code @ ");
+      Serial.println(millis());
+      morseCodeRepeater.resetTimer();
+      digitalWrite(OUTPUT_PIN, 1);  // Manually turn on the output in case CUTOFF_PERIOD_TIME has turned it off
+      delay(1000);
+      morseCodeRepeater.playMorseCode();
+      
+      // Simulate the morse code as the reciever recieving, so it gets the adequate buffer delay.
+      inval = 1;
   }
 
   // ---------- Buffer when turn off code here ----------
@@ -151,7 +179,7 @@ void loop() {
 
   // If we are less than the delay after a falling edge, keep the output high.
   // Otherwise, simply pass through the previous value unimpeded
-  if (start_time != 0 && millis() - start_time <= buffer_delay_time) {
+  if (start_time != 0 && millis() - start_time <= BUFFER_DELAY_TIME) {
     bufferval = 1;
   } else {
     bufferval = inval;
@@ -169,7 +197,7 @@ if(!last_bufferval && bufferval) {
   Serial.println(cutoff_start_time);
 }
 
-if (cutoff_start_time != 0 && cutoff_delay_time <= millis() - cutoff_start_time) {
+if (cutoff_start_time != 0 && CUTOFF_PERIOD_TIME <= millis() - cutoff_start_time) {
   cutoffval = 0;
 } else {
   cutoffval = bufferval; 
@@ -179,22 +207,21 @@ if (cutoff_start_time != 0 && cutoff_delay_time <= millis() - cutoff_start_time)
   // Output here
   outval = cutoffval;
 
-  // Output the final output to turn on transmitter
+  // Output the final output to turn on transmitter push-to-talk
+  // If we are turning off the transmitter, first play the Quindar tone if enabled
+  if (QUINDAR_ENABLED && last_outval && !outval) {
+    Serial.print("Playing Quindar Off Tone @ ");
+    Serial.println(millis());
+    tone(MORSE_PIN, QUINDAR_OFF_FREQUENCY);
+    delay(QUINDAR_OFF_DURATION);
+    noTone(MORSE_PIN);
+  }
+
   digitalWrite(OUTPUT_PIN, outval);
 
-
   last_inval = inval;
-  last_bufferval = bufferval; 
-  delay(100);
+  last_bufferval = bufferval;
+  last_outval = outval;
 
-  if (morseCodeRepeater.hasTimeElapsed()) {
-      Serial.print("Playing Morse Code @ ");
-      Serial.println(millis());
-      morseCodeRepeater.resetTimer();
-      digitalWrite(OUTPUT_PIN,1);
-      delay(1000);
-      morseCodeRepeater.playMorseCode();
-      delay(1000);
-      digitalWrite(OUTPUT_PIN,0);
-  }
+  delay(100);  // Loop at 10Hz
 }
